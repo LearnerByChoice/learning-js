b31213aedf6c4be96e8342cab7e8dfc48137f603
LearnerByChoice <ctrlaltelite378@gmail.com>
2025-10-21 18:43:55 +0530

    uncommenting first part of arrays

--- 02_BasicsLevelUp/04.Arrays.js
+++ 02_BasicsLevelUp/04.Arrays.js
@@ -1,55 +1,55 @@
-// // array - collection of multiple items in a single variable
+// array - collection of multiple items in a single variable
 
-// const myArr = ['Test1', 'Test2', 'Test3', 5, true, 79.5, 'TestUser5']
-// console.log(myArr); /* Output - [ 'Test1', 'Test2', 'Test3', 5, true, 79.5, 'TestUser5' ] */
+const myArr = ['Test1', 'Test2', 'Test3', 5, true, 79.5, 'TestUser5']
+console.log(myArr); /* Output - [ 'Test1', 'Test2', 'Test3', 5, true, 79.5, 'TestUser5' ] */
 
-// // array indexing starts from 0
+// array indexing starts from 0
 
-// // array copy operation performs shallow copy
-// // shallow copy of an object is a copy whose properties share the same reference (points to the same underlying values)
-// // that means if you change the copy you change the actual object as well
+// array copy operation performs shallow copy
+// shallow copy of an object is a copy whose properties share the same reference (points to the same underlying values)
+// that means if you change the copy you change the actual object as well
 
-// const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr2); /* Output - [ 1, 2, 3, 4 ] */
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr2); /* Output - [ 1, 2, 3, 4 ] */
 
-// // REMEMBER: Arrays have a prototype and an inner prototype as well
+// REMEMBER: Arrays have a prototype and an inner prototype as well
 
-// // Array methods:
-// myArr.push('TestUser6')
-// console.log(myArr); /* Output - [ 'Test1', 'Test2', 'Test3', 5, true, 79.5, 'TestUser5', 'TestUser6'] */
+// Array methods:
+myArr.push('TestUser6')
+console.log(myArr); /* Output - [ 'Test1', 'Test2', 'Test3', 5, true, 79.5, 'TestUser5', 'TestUser6'] */
 
-// myArr.pop() //does not take any argument
-// myArr.pop() 
-// console.log(myArr); /* Output after two pops - [ 'Test1', 'Test2', 'Test3', 5, true, 79.5 ] */
+myArr.pop() //does not take any argument
+myArr.pop() 
+console.log(myArr); /* Output after two pops - [ 'Test1', 'Test2', 'Test3', 5, true, 79.5 ] */
 
-// myArr.unshift('myunshiftvariable')
-// console.log(myArr); /* Output - [ 'myunshiftvariable', 'Test1', 'Test2', 'Test3', 5, true, 79.5 ] */
-// // unshift is not good, because it is memory extensive operation, as it shifts all variables
+myArr.unshift('myunshiftvariable')
+console.log(myArr); /* Output - [ 'myunshiftvariable', 'Test1', 'Test2', 'Test3', 5, true, 79.5 ] */
+// unshift is not good, because it is memory extensive operation, as it shifts all variables
 
-// myArr.shift() // mostly similar as pop() method, takes out first variable
-// myArr.shift()
-// console.log(myArr); /* Output - [ 'Test2', 'Test3', 5, true, 79.5 ] */
+myArr.shift() // mostly similar as pop() method, takes out first variable
+myArr.shift()
+console.log(myArr); /* Output - [ 'Test2', 'Test3', 5, true, 79.5 ] */
 
-// console.log(myArr.includes(79.5)); /* Output - true (boolean value) */
-// console.log(myArr.indexOf(9)); /* Output - -1 (because it does not exist) */
-// console.log(myArr.indexOf('Test3')); /* Output - 1 */
+console.log(myArr.includes(79.5)); /* Output - true (boolean value) */
+console.log(myArr.indexOf(9)); /* Output - -1 (because it does not exist) */
+console.log(myArr.indexOf('Test3')); /* Output - 1 */
 
-// // how to convert array to string
-// const newArr = myArr.join()
-// console.log(myArr); /* Output - [ 'Test2', 'Test3', 5, true, 79.5 ] */
-// console.log(typeof myArr); /* Output - object */
-// console.log(newArr); /* Output - Test2,Test3,5,true,79.5 */
-// console.log(typeof newArr); /* Output - string */
+// how to convert array to string
+const newArr = myArr.join()
+console.log(myArr); /* Output - [ 'Test2', 'Test3', 5, true, 79.5 ] */
+console.log(typeof myArr); /* Output - object */
+console.log(newArr); /* Output - Test2,Test3,5,true,79.5 */
+console.log(typeof newArr); /* Output - string */
 
-// // slice and splice - IMPORTANT for interview
-// console.log("This is my original array: ", myArr); /* Output - [ 'Test1', 'Test2', 'Test3', 5, true, 79.5, 'TestUser5' ] */
-// const slicedArray = myArr.slice(1, 3) // slice returns a section of an array
-// console.log("This is my sliced array: ", slicedArray); /* Output - [ 'Test2', 'Test3' ] */
-// console.log("This is my original array after slice operation: ", myArr); /* Output - [ 'Test1', 'Test2', 'Test3', 5, true, 79.5, 'TestUser5' ] */
-// console.log(`***************************************`)
-// const splicedArray = myArr.splice(1, 3) // splice includes the end index and manipulates the original array
-// console.log("This is my spliced array: ", splicedArray); /* Output - [ 'Test2', 'Test3', 5 ] */
-// console.log("This is my original array after splice operation: ", myArr); /* Output - [ 'Test1', true, 79.5, 'TestUser5' ] */
+// slice and splice - IMPORTANT for interview
+console.log("This is my original array: ", myArr); /* Output - [ 'Test1', 'Test2', 'Test3', 5, true, 79.5, 'TestUser5' ] */
+const slicedArray = myArr.slice(1, 3) // slice returns a section of an array
+console.log("This is my sliced array: ", slicedArray); /* Output - [ 'Test2', 'Test3' ] */
+console.log("This is my original array after slice operation: ", myArr); /* Output - [ 'Test1', 'Test2', 'Test3', 5, true, 79.5, 'TestUser5' ] */
+console.log(`***************************************`)
+const splicedArray = myArr.splice(1, 3) // splice includes the end index and manipulates the original array
+console.log("This is my spliced array: ", splicedArray); /* Output - [ 'Test2', 'Test3', 5 ] */
+console.log("This is my original array after splice operation: ", myArr); /* Output - [ 'Test1', true, 79.5, 'TestUser5' ] */
 
 
 // ******************************************************
